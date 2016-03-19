dac10427877dad3e17a298d9eddbee56c349fe7f
Colin Dunklau <colin.dunklau@gmail.com>
2016-03-19 17:57:44 -0500

    Separate startup concerns

--- athenicpaste/index.js
+++ athenicpaste/index.js
@@ -7,27 +7,40 @@ let createDatabaseManager = require('./models').createDatabaseManager;
 let ClientError = require('./errors.js').ClientError;
 
 
-function createApplication(config) {
+function createApplication(config, dbManager) {
+  let app = express();
+
+  app.use(function(request, response, next) {
+    request.db = dbManager;
+    next();
+  });
+
+  app.use(router);
+
+  app.use(function handleClientError(err, request, response, next) {
+    if (err instanceof ClientError) {
+      response.status(err.statusCode).send(err.message);
+    } else {
+      next(err);
+    }
+  });
+
+  return app;
+}
+exports.createApplication = createApplication;
+
+
+/**
+ * Return a Promise that resolves with a listening http.Server.
+ */
+function startServer(config) {
   return createDatabaseManager(config.dbUrl).then(function(dbManager) {
-    let app = express();
-
-    app.use(function(request, response, next) {
-      request.db = dbManager;
-      next();
-    });
-
-    app.use(router);
-
-    app.use(function handleClientError(err, request, response, next) {
-      if (err instanceof ClientError) {
-        response.status(err.statusCode).send(err.message);
-      } else {
-        next(err);
-      }
-    });
-
-    return app;
+    return createApplication(config, dbManager);
+  }).then(function(application) {
+    return application.listen(config.port);
+  }).catch(function(err) {
+    console.log('Failed to set up listener %s', err);
+    throw err;
   });
 }
-
-module.exports = createApplication;
+exports.startServer = startServer;
